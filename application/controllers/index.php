41c9e76b4b6a0c05bdecfe77e0f49663f913f3a0
PawelSz <peal1982@gmail.com>
2015-04-08 18:09:22 +0200

    Popraw wyswietlenie pojedynczego obrazu

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -128,6 +128,7 @@ include 'wyswietl_tresc_trait.php';
 //////////////////// CREATE URL helpers /////////////////////////////////////////////	
 	var url_pliku_glownego ="index.php"
 	var url_do_podstrony = url_pliku_glownego+"/index/index"
+	var url_do_JSON = "index.php/"+"pobierz_info_o_obrazie_JSON" + "/index/";
 
 	var page_url = window.location.href;
 	var base_url =  page_url.substr(0, page_url.lastIndexOf(url_pliku_glownego));
@@ -138,23 +139,32 @@ include 'wyswietl_tresc_trait.php';
 
 //////////////////// End of CREATE URL helpers /////////////////////////////////////////////
 
-var utworz_div_obrazu = function(adres, alt) {
+var utworz_div_obrazu = function(adres, alt, url) {
+	var $link = $("<a></a>");
+	$link.attr("href", url);
+	
 	var $obraz = $("<img>");
 	$obraz.attr("src", adres).attr("class", "zdjecie_srednie");
 	if ( !( typeof alt === "undefined" || alt === null )) {
 		$obraz.attr("alt", alt);
 	}
+	
+	$link.append($obraz);
+	return "<div class='image_div'>"+$link.get(0).outerHTML+"</div>";	
 
-	return "<div class='image_div'>"+$obraz.get(0).outerHTML+"</div>";	
+	// return "<div class='image_div'>"+$obraz.get(0).outerHTML+"</div>";	
 }
 
 var wyswietl_obraz = function(row, data) {
-	//console.log("data.id " + data[0]);
+	var url = base_url + url_do_podstrony +"/"+ data.nazwa_pliku;
+
 	if (data.czy_lokalny === "1") {
-		var div =  utworz_div_obrazu( base_url +"image/"+ data.nazwa_pliku+"."+ data.rozszerzenie, data.alt_text );
+		var adres_zdjecie = base_url +"image/"+ data.nazwa_pliku+"."+ data.rozszerzenie;
 	} else {
-		var div =  utworz_div_obrazu( data.url_pliku, data.alt_text );
+		var adres_zdjecie = data.url_pliku;
 	};
+	var div =  utworz_div_obrazu( adres_zdjecie, data.alt_text, url);
+
 	$("#tresc").append(div);
 };
 
@@ -164,17 +174,18 @@ var wyswietl_obrazy = function(data) {
 
 
 $(document).ready(function(){
-	console.log("page_url", page_url)
-	console.log("subpage "+url_subpage);
-	console.log("url_subpage", url_subpage);
-	console.log("koncowka_url", koncowka_url);
-	console.log("url_do_podstrony", url_do_podstrony);
-	console.log("podstrona", podstrona);
+	// console.log("page_url", page_url);
+	// console.log("base_url", base_url);
+	// console.log("subpage "+url_subpage);
+	// console.log("url_subpage", url_subpage);
+	// console.log("koncowka_url", koncowka_url);
+	// console.log("url_do_podstrony", url_do_podstrony);
+	// console.log("podstrona", podstrona);
 
 	if (koncowka_url == url_pliku_glownego || koncowka_url == url_pliku_glownego+"/" || koncowka_url == url_pliku_glownego + "/index" ){
 		//glowna strona
 		console.log("glowna_strona");
-		$.getJSON(base_url + "index.php/" +"pobierz_info_o_obrazie_JSON", function(data){
+		$.getJSON(base_url + url_do_JSON, function(data){
 		wyswietl_obrazy(data);
 	}).fail(function( jqxhr, textStatus, error ) {
                     var err = textStatus + ', ' + error;
@@ -183,9 +194,9 @@ $(document).ready(function(){
 	} else {
 		//podstrona
 		console.log("podstrona");
-		html_do_przeslania = base_url + "index.php/"+"pobierz_info_o_obrazie_JSON" + "/index/"+ podstrona;
+		html_do_przeslania = base_url + url_do_JSON+ podstrona;
 		$.getJSON(html_do_przeslania, function(data){
-			
+			document.title = data[0].nazwa_pliku;
 			wyswietl_obrazy(data);
 			//$.each(data, wyswietl_obrazy);
 
@@ -195,9 +206,6 @@ $(document).ready(function(){
          });
 	}
 
-
-
-	//console.log("ready2");
 })
 
 </script>
