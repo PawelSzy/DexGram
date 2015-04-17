9aa6e7c6be5907fdc09d63b293889ec368eda358
PawelSz <peal1982@gmail.com>
2015-04-17 21:40:19 +0200

    Popraw bledy z bocznym paskiem

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -119,6 +119,8 @@ include 'wyswietl_tresc_trait.php';
 ?>			
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" >
 </script>
-<script type="text/javascript" src="js/DexGram.js">
+<!--
+<script type="text/javascript" src="/js/DexGram.js">
 
 </script>
+-->
